e76f1aab40aa4833557cf16bbf105ab8d3602186
Gianluca Orpello <g.orpello@gmail.com>
2021-03-19 16:45:18 +0100

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Gianluca Orpello on 19/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
